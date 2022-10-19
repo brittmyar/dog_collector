889121d93fa3f237d38256e4091408399b78f414
Brittany Yarbrough <brittanymyar@gmail.com>
2022-10-19 14:35:34 -0400

    set up admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Dog
+
 # Create your views here.
 
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
 
-dogs = [
-  Dog('Blu', 'great dane', 'sweet lil baby', 6),
-  Dog('Auggy', 'vizslador', 'absolute madman and scruffy', 3),
-  Dog('Torree', 'pit-bull', 'sweet lion', 7)
-]
 
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -22,4 +13,9 @@ def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog': dog })
