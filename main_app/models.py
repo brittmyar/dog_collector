dbe7b7fd8eaa68948b92bb1d783c5a3ae007ea77
Brittany Yarbrough <brittanymyar@gmail.com>
2022-10-19 23:05:13 -0400

    create toys

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,51 @@
 from django.db import models
+from django.urls import reverse
+
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
 
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
-age = models.IntegerField()
-def __str__(self):
+    def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
+
+
+class Feeding(models.Model):
+    date = models.DateField('feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Dog
-
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from .models import Dog, Toy
+from .forms import FeedingForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 
-
-
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
@@ -18,4 +18,61 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', { 'dog': dog })
+  # instantiate FeedingForm to be rendered in the template
+  feeding_form = FeedingForm()
+
+  # displaying unassociated toys
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+
+  return render(request, 'dogs/detail.html', {
+    # include the dog and feeding_form in the context
+    'dog': dog,
+    'feeding_form': feeding_form,
+    'toys' : toys_dog_doesnt_have,
+  })
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
+
+def assoc_toy(request, dog_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+   Dog.objects.get(id=dog_id).toys.add(toy_id)
+   return redirect('detail', dog_id=dog_id)
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'description', 'age',]
+
+class DogUpdate(UpdateView):
+  model = Dog
+  # Let's disallow the renaming of a Dog by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ('name', 'color')
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ('name', 'color')
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+class ToyDetail(DetailView):
+    model = Toy
+    template_name = 'toys/detail.html'
+
+class ToyList(ListView):
+    model = Toy
+    template_name = 'toys/index.html'
